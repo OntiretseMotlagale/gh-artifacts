897c33b635545a833a58d9f45977a7a9fcb08ce7
ontiretsemmotlagale <ontiretsemotlagale@gmail.com>
2026-05-27 21:40:51 +0200

    Added artifacts

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h3>Learning about artifacts</h3>
       </header>
       <MainContent />
     </>
